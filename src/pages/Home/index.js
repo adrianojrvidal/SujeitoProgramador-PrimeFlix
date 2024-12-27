c6e3b53641a2080e67dcb2e0e746beb1b97a7cf0
Adriano Junior Vidal <adriano.jv@hotmail.com>
2024-12-26 22:43:02 -0300

    Página Home
    API
    Listagem dos filmes
    Css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,46 @@
+import {useEffect, useState} from 'react';
+import api from '../../services/api';
+import {Link} from 'react-router-dom';
+
+import './home.css';
 
 function Home () {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+
+    async function loadFilmes() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: '9eead1f96ea5e4faf83f9f167b53f99c',
+          language: 'pt-BR',
+          page: 1
+        }
+      })
+
+      setFilmes(response.data.results.slice(0, 10))
+    };
+
+    loadFilmes();
+
+  }, [])
+
   return (
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+    <div className='container'>
+      <div className='lista-filmes'>
+        {
+          filmes.map((filme)=>{
+            return (
+              <article key={filme.id}>
+                <strong>{filme.title}</strong>
+                <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                <Link to={`/filme/${filme.id}`}>Acessar</Link>
+
+              </article>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
